2660385b424d43b50414dcc29c11c02a7b72d91b
Anthonius Munthi <me@itstoni.com>
2021-09-08 13:25:53 +0800

    ci-integration

--- api/src/User/Model/User.php
+++ api/src/User/Model/User.php
@@ -16,56 +16,40 @@ class User
     protected string $id;
     protected string $username;
     protected string $email;
-    protected string $password;
-    protected string $plainPassword;
+    protected ?string $password;
+    protected ?string $plainPassword;
 
     public function __construct(
         string $username,
         string $email,
-        string $plainPassword
-    )
-    {
-        $this->username = $username;
-        $this->email = $email;
+        ?string $plainPassword = null
+    ) {
+        $this->username      = $username;
+        $this->email         = $email;
         $this->plainPassword = $plainPassword;
     }
 
-    /**
-     * @return string
-     */
     public function getId(): string
     {
         return $this->id;
     }
 
-    /**
-     * @return string
-     */
     public function getUsername(): string
     {
         return $this->username;
     }
 
-    /**
-     * @return string
-     */
     public function getEmail(): string
     {
         return $this->email;
     }
 
-    /**
-     * @return string
-     */
-    public function getPassword(): string
+    public function getPassword(): ?string
     {
         return $this->password;
     }
 
-    /**
-     * @return string
-     */
-    public function getPlainPassword(): string
+    public function getPlainPassword(): ?string
     {
         return $this->plainPassword;
     }
